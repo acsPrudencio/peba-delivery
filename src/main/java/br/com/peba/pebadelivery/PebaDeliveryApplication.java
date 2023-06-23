62008dba39f2774aae72f3bf8ae0887dab67df9e
Antonio Carlos <acsprudencio@gmail.com>
2023-06-23 08:50:55 -0300

    Initial Commit

---
+++ src/main/java/br/com/peba/pebadelivery/PebaDeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.peba.pebadelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PebaDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PebaDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/peba/pebadelivery/PebaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.peba.pebadelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PebaDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
